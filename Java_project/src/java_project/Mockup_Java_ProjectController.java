6009a9f33541c9c220c04702a929b25efe9e7528
Guth Pierre <pierreguth@viacesi.fr>
2014-02-05 09:50:39 +0100

    no message

--- Java_project/src/java_project/Mockup_Java_ProjectController.java
+++ Java_project/src/java_project/Mockup_Java_ProjectController.java
@@ -6,9 +6,20 @@
 
 package java_project;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,13 +27,136 @@ import javafx.fxml.Initializable;
  * @author Pierre
  */
 public class Mockup_Java_ProjectController implements Initializable {
-
+    private double a, b;
+    
+    
+    
+    @FXML
+    private Button newsButton;
+    @FXML
+    private Button consultButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private SplitPane newsSplitPanel;
+    @FXML
+    private Accordion mainAccordion;
+    @FXML
+    private ImageView mapImage;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //Image imageMap = new Image("file:jpg\\test_map.jpg");
+        
+        //NOOB
+        //NOOB
+        //NOOB
+        //NOOB
+        //NOOB
+        //NOOB
+        //NOOB
+        //:)
+        Image imageMap = new Image(getClass().getResourceAsStream("/java_project/jpg/test_map.png"));
+        //Image imageMap = new Image(getClass().getResource("jpg\\test_map.jpg").toExternalForm());
+        
+        mapImage.setFitHeight(imageMap.getHeight());
+        mapImage.setFitWidth(imageMap.getWidth());
+        mapImage.setImage(imageMap);
+        //mapImage.setTranslateX(-100);
+        //mapImage.setTranslateY(-200);
+        
+        
+        
+        /*mapImage.setOnDragDetected(new EventHandler<MouseEvent>() {
+           @Override
+           public void handle(MouseEvent event) {
+               a = event.getX();
+               System.out.println("X = " + a);
+               b = event.getY();
+               System.out.println("Y = " + b);
+               
+               //mapImage.setTranslateX(event.getX());
+               //mapImage.setTranslateY(event.getY());
+           }
+        });*/
+        
+        mapImage.setOnMousePressed(new EventHandler<MouseEvent>() {
+            
+            @Override
+            public void handle(MouseEvent event) {
+                a = event.getX();
+                b = event.getY();
+                System.out.println("press X = " + event.getX());
+                System.out.println("press Y = " + event.getY());
+                
+            }
+        });
+        
+        mapImage.setOnMouseMoved(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if(event.isPrimaryButtonDown()) {
+                            System.out.println("release X = " + event.getX());
+                            System.out.println("release Y = " + event.getY());
+                            mapImage.setTranslateX(mapImage.getTranslateX() + (event.getX() - a));
+                            System.out.println("translate X = " + (event.getX() - a));
+                            mapImage.setTranslateY(mapImage.getTranslateY() + (event.getY() - b));
+                            System.out.println("translate Y = " + (event.getY() - b));
+                        
+                        }
+                    }
+                });
+        
+        mapImage.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("release X = " + event.getX());
+                System.out.println("release Y = " + event.getY());
+                mapImage.setTranslateX(mapImage.getTranslateX() + (event.getX() - a));
+                System.out.println("translate X = " + (event.getX() - a));
+                mapImage.setTranslateY(mapImage.getTranslateY() + (event.getY() - b));
+                System.out.println("translate Y = " + (event.getY() - b));
+            }
+        });
+        
+        /*mapImage.setOnDragDropped(new EventHandler<DragEvent>() {
+           
+           public void handle(DragEvent event) {
+               System.out.println("X = " + event.getX());
+               System.out.println("Y = " + event.getY());
+               mapImage.setTranslateX(a - event.getX());
+               mapImage.setTranslateY(b - event.getY());
+           }
+        });*/
+        
+        
+        
+        
+        
+        newsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                newsSplitPanel.setVisible(true);
+                mainAccordion.setVisible(false);
+            }
+        });
+        
+        consultButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                newsSplitPanel.setVisible(false);
+                mainAccordion.setVisible(true);
+            }
+        });
+        
+        editButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("That was easy, wasn't it?");
+            }
+        });
     }    
     
 }
--- Java_project/src/java_project/NewFXMain.java
+++ Java_project/src/java_project/NewFXMain.java
@@ -9,11 +9,8 @@ package java_project;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,17 +18,28 @@ import javafx.stage.Stage;
  *
  * @author Pierre
  */
-public class Java_project extends Application {
+public class NewFXMain extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Mockup_Java_Project.fxml"));
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
         
-        Scene scene = new Scene(root);
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
         
-        stage.setScene(scene);
-        stage.setTitle("Colossal_Java_Project");
-        stage.show();
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
