317269fc9b8743e92462edbdc878d4fb9e0c0a99
Guth Pierre <pierre.guth@viacesi.fr>
2014-02-08 20:46:51 +0100

    marker

--- ProjetJava/src/projetjava_2/presentation/Marker.java
+++ ProjetJava/src/projetjava_2/presentation/Marker.java
@@ -6,20 +6,61 @@
 
 package projetjava_2.presentation;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+
 /**
  *
  * @author Lakio
  */
-public class Marker {
+public class Marker{
     
     private Integer id;
+    public Polygon triangle;
+    public AnchorPane mapContainer;
     
-    public Marker(Integer id)
+    public Marker(Integer id, AnchorPane mapcontainer, String libelle)
     {
+        this.mapContainer = mapcontainer;
         this.id = id;
+        triangle = new Polygon();
+        triangle.setOpacity(0.8);
+        triangle.setFill(Paint.valueOf("Red"));
+        triangle.setTranslateX(740);
+        triangle.setTranslateY(420);
+        triangle.getPoints().addAll(new Double[]{
+        0.0, 0.0,
+        -10.0, -30.0,
+        10.0, -30.0});
+        
+        final Label label = new Label(libelle);
+        label.setTranslateX(triangle.getTranslateX());
+        label.setTranslateY(triangle.getTranslateY());
+        label.setStyle("-fx-background-color: white;-fx-border-width: 3; -fx-border-color: black; -fx-padding: 2 2 2 2; -fx-font-size: 18; -fx-font-weight: bold; -fx-border-radius: 5");
+        
+        triangle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mapContainer.getChildren().add(2, label);
+            }
+        });
+        
+        triangle.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mapContainer.getChildren().remove(label);
+            }
+        });
+        
+        mapContainer.getChildren().add(1,triangle);
     }
     
-    public Integer getId(){return id;}
+    
+    public Integer getIdmarker(){return id;}
     
     
     
